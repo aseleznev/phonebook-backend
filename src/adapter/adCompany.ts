5a4d13ac9de4bb20521405d0e81d6a888e7f70c1
aseleznev <aa.seleznev@gmail.com>
2020-05-06 11:30:50 +0500

    drop schema true

--- src/adapter/adCompany.ts
+++ src/adapter/adCompany.ts
@@ -1,5 +1,5 @@
-import {ADPerson} from "./adPerson";
-import {ADNode} from "./adNode";
+import { ADPerson } from './adPerson';
+import { ADNode } from './adNode';
 
 export class ADCompany {
     name: string;
@@ -11,43 +11,43 @@ export class ADCompany {
     address: string;
 
     titleWeight = new Map([
-        ["Генеральный", 1],
+        ['Генеральный', 1],
 
-        ["Заместитель генерального директора", 2],
-        ["Главный бухгалтер", 2],
-        ["Начальник филиала", 2],
+        ['Заместитель генерального директора', 2],
+        ['Главный бухгалтер', 2],
+        ['Начальник филиала', 2],
 
-        ["Заместитель главного", 3],
-        ["Заместитель начальника филиала", 3],
+        ['Заместитель главного', 3],
+        ['Заместитель начальника филиала', 3],
 
-        ["Начальник управления", 3],
-        ["Начальник центра", 3],
-        ["Начальник службы", 3],
+        ['Начальник управления', 3],
+        ['Начальник центра', 3],
+        ['Начальник службы', 3],
 
-        ["Заместитель начальника центра", 4],
-        ["Заместитель начальника управления", 4],
-        ["Заместитель начальника службы", 4],
+        ['Заместитель начальника центра', 4],
+        ['Заместитель начальника управления', 4],
+        ['Заместитель начальника службы', 4],
 
-        ["Начальник отдела", 5],
-        ["Заместитель начальника отдела", 6],
+        ['Начальник отдела', 5],
+        ['Заместитель начальника отдела', 6],
 
-        ["Главный механик", 5],
-        ["Главный энергетик", 5],
+        ['Главный механик', 5],
+        ['Главный энергетик', 5],
 
-        ["Руководитель группы", 7],
-        ["Начальник лаборатории", 8],
+        ['Руководитель группы', 7],
+        ['Начальник лаборатории', 8],
 
-        ["Главный специалист", 9],
-        ["Главный юрисконсульт", 9],
-        ["Главный технолог", 9],
-        ["Главный аудитор", 9],
-        ["Ведущий", 10],
-        ["Техник", 12]
+        ['Главный специалист', 9],
+        ['Главный юрисконсульт', 9],
+        ['Главный технолог', 9],
+        ['Главный аудитор', 9],
+        ['Ведущий', 10],
+        ['Техник', 12]
     ]);
 
     divisionWeight = new Map([
-        ["Руководство", 1],
-        ["Аппарат при руководстве", 2]
+        ['Руководство', 1],
+        ['Аппарат при руководстве', 2]
     ]);
 
     constructor(name: string, phone?: string, gazPhone?: string, fax?: string, mail?: string, address?: string) {
@@ -68,13 +68,13 @@ export class ADCompany {
         //
 
         // Хардкод для учеток, у которых нет ни отдела, ни управления
-        if (!person.departmentNumber && !person.division/* && !person.description*/) return 0;
+        if (!person.departmentNumber && !person.division /* && !person.description*/) return 0;
         //
 
         let dep = null;
         if (person.division) {
             // Хак для НТЦ
-            if (person.departmentNumber === "Руководство" && person.division !== "Руководство")
+            if (person.departmentNumber === 'Руководство' && person.division !== 'Руководство')
                 person.departmentNumber = person.division;
             dep = this.addDivision(person.division);
         }
@@ -85,9 +85,8 @@ export class ADCompany {
 
         if (person.division && person.departmentNumber) {
             if (person.division === person.departmentNumber) {
-                dep = this.addDivision(person.division)
-            } else
-                dep = this.addDepartmentNumber(person.departmentNumber, person.division);
+                dep = this.addDivision(person.division);
+            } else dep = this.addDepartmentNumber(person.departmentNumber, person.division);
         }
 
         if (person.description && person.departmentNumber && person.division) {
@@ -95,9 +94,8 @@ export class ADCompany {
         }
 
         if (!person.division && person.departmentNumber && person.description) {
-            dep = this.addDescription(person.description, person.departmentNumber)
+            dep = this.addDescription(person.description, person.departmentNumber);
         }
-
 
         // this.addDivision(person.division);
         //
@@ -126,10 +124,8 @@ export class ADCompany {
                 const p = new ADPerson();
                 p.cn = person.cn;
                 p.title = person.title;
-                if (person.cn !== "Белюскин Евгений Владимирович")
-                    p.mail = person.mail;
-                else
-                    p.mail = "";
+                if (person.cn !== 'Белюскин Евгений Владимирович') p.mail = person.mail;
+                else p.mail = '';
                 p.telephoneNumber = person.telephoneNumber;
                 p.mobile = person.mobile;
                 p.ipPhone = person.ipPhone;
@@ -149,9 +145,8 @@ export class ADCompany {
                 console.log(person);
                 throw new Error('ERROR!!!!!!!!!!!!');
             }
-
         } else {
-            console.log("addPerson IF:");
+            console.log('addPerson IF:');
             console.log(person);
             throw new Error('ERROR!!!!!!!!!!!!');
         }
@@ -186,7 +181,7 @@ export class ADCompany {
             });
             return _adNode;
         } else if (this.getDivisionByName(division)) {
-            return this.getDivisionByName(division)
+            return this.getDivisionByName(division);
         } else {
             return null;
         }
@@ -249,7 +244,6 @@ export class ADCompany {
         //         return this.getDivisionByName(departmentNumber);
         //     else
         //         return this.getDepartmentNumberByName(departmentNumber);
-
     }
 
     private addDescription(description: string, departmentNumber: string, division?: string): ADNode {
@@ -264,9 +258,9 @@ export class ADCompany {
             try {
                 dep.child.push(desc);
             } catch (e) {
-                console.log("ERROR addDescription: ");
+                console.log('ERROR addDescription: ');
                 console.log(dep);
-                console.log("\n");
+                console.log('\n');
                 console.log(desc);
                 console.log(`${description} - ${departmentNumber} - ${division}`);
             }
